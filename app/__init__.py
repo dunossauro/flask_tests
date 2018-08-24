d569ff8c25cc849ac73fd20dd73b5ac8ea768cfa
dunossauro <mendesxeduardo@gmail.com>
2018-08-24 14:39:52 -0300

    WIP - Testes com rotas e mock

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,21 @@
 from flask import Flask, request, jsonify, abort
 
+tasks_queue = []
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route('/send_task', methods=['PUT'])
+def put_todo():
+    data = request.json
+    if 'task' in data:
+        tasks_queue.append(data['task'])
+
+        return jsonify('Aceppted'), 201
+
+    abort(300, 'Você não enviou a task')
